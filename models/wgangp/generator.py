cd01eb5e08bd996dd21fc05c3362ad7e5904e284
Achraf LOUIZA <achraf.louiza97@gmail.com>
2023-03-23 15:01:28 +0100

    Implementing generator for WGANGP

---
+++ models/wgangp/generator.py
@@ -0,0 +1,32 @@
+from tensorflow import keras
+from tensorflow.keras.layers import (
+    Input, Conv2D, Dense, Dropout,
+    Conv2DTranspose, MaxPooling2D, UpSampling2D, 
+    Reshape, LeakyReLU, BatchNormalization
+)
+from tensorflow.keras.models import Model
+
+class Generator:
+    '''
+    A convolution generator for DCGAN
+    '''
+    
+    def __init__(self, latent_dim, img_shape):
+        
+        n_debut = img_shape[0]//2**3
+        inputs = Input(shape=latent_dim)
+        
+        x = Dense(n_debut*n_debut*256)(inputs)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = Reshape((n_debut, n_debut, 256))(x)
+        
+        x = Conv2DTranspose(128, kernel_size=5, strides=2, padding='same')(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        
+        x = Conv2DTranspose(64, kernel_size=5, strides=2, padding='same')(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        
+        output = Conv2DTranspose(3, kernel_size=5, strides=2, padding='same', activation='tanh')(x)
+        
+        self.model = Model(inputs, output, name='conv-generator')
+    
