28ab9ed4818a0fc7645551d7d148615c656a6217
Achraf LOUIZA <achraf.louiza97@gmail.com>
2023-03-23 16:09:24 +0100

    Enlarging the discriminator layers

---
+++ models/wgangp/discriminator.py
@@ -0,0 +1,32 @@
+from tensorflow import keras
+from tensorflow.keras.layers import (
+    Input, Conv2D, Dense, Dropout,
+    Flatten, Conv2DTranspose, MaxPooling2D, 
+    UpSampling2D, Reshape, BatchNormalization, LeakyReLU
+)
+from tensorflow.keras.models import Model
+
+class Discriminator:
+    '''
+    A deconvolution discriminator for DCGAN
+    '''
+    def __init__(self, img_shape):
+        
+        inputs = Input(shape=img_shape)
+        
+        x = Conv2D(64, kernel_size=5, strides=2, padding='same')(inputs)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = Dropout(0.2)(x)
+        
+        x = Conv2D(128, kernel_size=5, strides=2, padding='same')(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = Dropout(0.2)(x)
+        
+        x = Conv2D(256, kernel_size=5, strides=2, padding='same')(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = Dropout(0.2)(x)
+        
+        x = Flatten()(x)
+        output = Dense(1)(x)
+        
+        self.model = Model(inputs, output, name='deconv-discriminator')
